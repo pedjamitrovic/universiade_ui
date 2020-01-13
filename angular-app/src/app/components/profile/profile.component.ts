a656fda79a3d45426364e20582247b50eaa1bc3a
pedjamitrovic <pre11mit@yahoo.com>
2020-01-13 16:24:17 +0100

    Profile ui done

--- angular-app/src/app/components/profile/profile.component.ts
+++ angular-app/src/app/components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,10 +8,18 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
+  profileForm: FormGroup;
   constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.profileForm = new FormGroup({
+      firstName: new FormControl(this.userService.user.firstName, [Validators.required]),
+      lastName: new FormControl(this.userService.user.lastName, [Validators.required]),
+      phoneNumber: new FormControl(this.userService.user.phoneNumber, [Validators.required]),
+      homeAddress: new FormControl(this.userService.user.homeAddress, [Validators.required]),
+      username: new FormControl(this.userService.user.username, [Validators.required]),
+      password: new FormControl(this.userService.user.password, [Validators.required]),
+    });
   }
 
 }
